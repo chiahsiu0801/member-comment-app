dbeb8d05546af66a130535e09d18bf5e003a307f
chiahsiu0801 <zyzhangtom870801@gmail.com>
2024-02-19 20:01:06 +0800

    Change to hashrouter again

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename='/member-comment-app'>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
