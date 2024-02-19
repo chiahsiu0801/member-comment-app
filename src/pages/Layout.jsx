fb3b31099d715b407a27f7ecbdcb7b5cad3fe0f6
chiahsiu0801 <zyzhangtom870801@gmail.com>
2024-02-19 14:47:53 +0800

    Connect to a MongoDB database with Mongoose

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,8 +6,7 @@ export default function Layout() {
   return (
     <>
       <div className="min-h-screen relative top-0 left-0 w-full">
-        {/* <img src="./../public/miko.jpg" alt="" className="w-full h-full bg-cover absolute opacity-60" /> */}
-        <img src="" alt="" className="w-full h-full bg-cover absolute opacity-60" />
+        <img src="./../public/miko.jpg" alt="" className="w-full h-full bg-cover absolute opacity-60" />
         <div className="w-full min-h-screen bg-cover bg-center flex flex-col items-center justify-center absolute">
           <Outlet></Outlet>
         </div>
